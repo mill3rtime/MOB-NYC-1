894b3ffc85eb5b23a1704305aea8699c7a0b98a5
Matt Miller <mrmiller912@gmail.com>
2015-02-15 15:49:44 -0500

    calculator commit in case of natural disaster :)

---
+++ Assesments/Midterm/CalculatorM/CalculatorM/CalcButton.swift
@@ -0,0 +1,9 @@
+//
+//  CalcButton.swift
+//  CalculatorM
+//
+//  Created by Matt Miller on 2/12/15.
+//  Copyright (c) 2015 Matt. All rights reserved.
+//
+
+import Foundation
---
+++ Assesments/Midterm/CalculatorM/CalculatorM/Calculator.swift
@@ -0,0 +1,9 @@
+//
+//  Calculator.swift
+//  CalculatorM
+//
+//  Created by Matt Miller on 2/13/15.
+//  Copyright (c) 2015 Matt. All rights reserved.
+//
+
+import Foundation
